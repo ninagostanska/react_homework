91db273236ea60513ce640176343f509aa94ec42
ninagostanska <ninagorska@gmail.com>
2018-05-05 08:37:17 +0200

    An Interactive Component

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ registerServiceWorker();
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-            {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
             </button>
     );
     }
