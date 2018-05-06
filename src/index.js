48f46f35650787b1554b44c8bac1bab568e1bd5f
ninagostanska <ninagorska@gmail.com>
2018-05-06 08:07:05 +0200

    Lifting State Up

--- src/index.js
+++ src/index.js
@@ -9,27 +9,37 @@ ReactDOM.render( < App / >, document.getElementById('root')
 ;
 registerServiceWorker();
 
-
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
     </button>
     );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+        />
+    );
     }
 
     render() {
@@ -39,19 +49,13 @@ class Board extends React.Component {
             <div>
             <div className="status">{status}</div>
             <div className="board-row">
-            {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
     </div>
         <div className="board-row">
-            {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
     </div>
         <div className="board-row">
-            {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
     </div>
         </div>
     );
